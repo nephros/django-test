c6eebf743c82892eb59c20ccd98448ffd337a02c
olf <Olf0@users.noreply.github.com>
2023-12-02 23:02:55 +0100

    [models.py] Add 4.5.0.24
    Reference: https://forum.sailfishos.org/t/release-notes-struven-ketju-4-5-0-24/16641/

--- example/demo/models.py
+++ example/demo/models.py
@@ -70,6 +70,7 @@ class FilesModel(models.Model):
 
     compatible_choices_default = "4.4.0.72"
     compatible_choices = (
+        ("4.5.0.24", "4.5.0.24"),
         ("4.5.0.21", "4.5.0.21"),
         ("4.5.0.19", "4.5.0.19"),
         ("4.5.0.18", "4.5.0.18"),
